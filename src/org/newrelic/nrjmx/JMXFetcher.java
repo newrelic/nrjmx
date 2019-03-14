c019eef8f0a536af5d391c88041e222f72e618e9
Ferran Orriols <forriols@newrelic.com>
2019-03-14 12:32:33 +0100

    IHOST-1609 added remoting-jmx url connection format

--- src/org/newrelic/nrjmx/JMXFetcher.java
+++ src/org/newrelic/nrjmx/JMXFetcher.java
@@ -41,8 +41,12 @@ public class JMXFetcher {
         public ValueError(String message) { super(message); }
     };
 
-    public JMXFetcher(String hostname, int port, String username, String password , String keyStore, String keyStorePassword, String trustStore, String trustStorePassword) throws ConnectionError {
+    public JMXFetcher(String hostname, int port, String username, String password , String keyStore, String keyStorePassword, String trustStore, String trustStorePassword, Boolean isRemote) throws ConnectionError {
         String connectionString = String.format("service:jmx:rmi:///jndi/rmi://%s:%s/jmxrmi", hostname, port);
+        if (isRemote) {
+            connectionString = String.format("service:jmx:remoting-jmx://%s:%s", hostname, port);
+        }
+
         Map<String, String[]> env = new HashMap<>();
         if (username != "") {
           env.put(JMXConnector.CREDENTIALS, new String[] { username, password });
