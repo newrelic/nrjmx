e3379f1ebab5e71a85b36d1a9b68b3ae252becee
Ramon Guiu <ramon.guiu@gmail.com>
2020-07-19 18:20:18 +0200

    Change license to Apache 2.0, update repo to follow new standards

--- src/main/java/org/newrelic/nrjmx/Application.java
+++ src/main/java/org/newrelic/nrjmx/Application.java
@@ -1,3 +1,8 @@
+/*
+* Copyright 2020 New Relic Corporation. All rights reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
+
 package org.newrelic.nrjmx;
 
 import org.apache.commons.cli.HelpFormatter;
