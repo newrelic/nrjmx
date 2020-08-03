4b08e2c9b4c9c724aa686773ab2311867a7a1794
Schalk W. Cronjé <ysb33r@gmail.com>
2020-08-03 09:37:14 +0200

    Convert build.gradle -> build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("http://repo.maven.apache.org/maven2")
+    }
+}
+
+dependencies {
+    implementation("commons-cli:commons-cli:1.4")
+    implementation("com.google.code.gson:gson:2.8.0")
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.testcontainers:testcontainers:1.11.3")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("nrjmx") {
+            from(components["java"])
+        }
+    }
+}
