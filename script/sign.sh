eaf29cb5a85c2cfaa40991d3f5979ade149789ba
Carlos <croman@newrelic.com>
2020-10-23 14:55:08 +0100

    ci: Added test and package steps to Github Actions (#61)
    * doc: Added ISSUE_TEMPLATE content that was missing
    * ci: Release workflow
    * release: Bumped version of project to 1.6.0

---
+++ script/sign.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+set -e
+
+#
+#
+#
+# Setup GPG key for GH Release Assets
+#
+#
+#
+
+
+echo "===> Making sure we can connect to gpg-agent"
+export GPG_TTY=$(tty)
+
+echo "===> Create .rpmmacros to sign rpm's from Goreleaser"
+echo "%_gpg_name ${GPG_MAIL}" >> ~/.rpmmacros
+echo "%_signature gpg" >> ~/.rpmmacros
+echo "%_gpg_path ${HOME}/.gnupg" >> ~/.rpmmacros
+echo "%_gpgbin /usr/bin/gpg" >> ~/.rpmmacros
+echo "%__gpg_sign_cmd   %{__gpg} gpg --no-verbose --no-armor --batch --pinentry-mode loopback --passphrase ${GPG_PASSPHRASE} --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} %{__plaintext_filename}" >> ~/.rpmmacros
+
+echo "===> Importing GPG private key from GHA secrets..."
+printf %s ${GPG_PRIVATE_KEY_BASE64} | base64 -d | gpg --batch --import -
+
+echo "===> Importing GPG signature"
+gpg --export -a ${GPG_MAIL} > /tmp/RPM-GPG-KEY-${GPG_MAIL}
+rpm --import /tmp/RPM-GPG-KEY-${GPG_MAIL}
+
+
+echo "===> Move to build distributions dir"
+cd build/distributions
+
+# Sign RPM's
+for rpm_file in $(find -regex ".*\.\(rpm\)");do
+  echo "===> Signing $rpm_file"
+  rpm --addsign $rpm_file
+  echo "===> Sign verification $rpm_file"
+  rpm -v --checksig $rpm_file
+done
+
+# Sign DEB's
+GNUPGHOME="${HOME}/.gnupg"
+echo "${GPG_PASSPHRASE}" > "${GNUPGHOME}/gpg-passphrase"
+echo "passphrase-file ${GNUPGHOME}/gpg-passphrase" >> "$GNUPGHOME/gpg.conf"
+echo 'allow-loopback-pinentry' >> "${GNUPGHOME}/gpg-agent.conf"
+echo 'pinentry-mode loopback' >> "${GNUPGHOME}/gpg.conf"
+echo 'use-agent' >> "${GNUPGHOME}/gpg.conf"
+echo RELOADAGENT | gpg-connect-agent
+
+for deb_file in $(find -regex ".*\.\(deb\)");do
+  echo "===> Signing $deb_file"
+  debsigs --sign=origin --verify --check -v -k ${GPG_MAIL} $deb_file
+done
