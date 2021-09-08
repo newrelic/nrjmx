413d109927b31a829b49a4afa9b43f3fcbcdaab1
cristianciutea <cristi.ciutea@gmail.com>
2021-09-08 10:53:41 +0200

    ci: fix typo

--- upload_artifacts_gh.sh
+++ upload_artifacts_gh.sh
@@ -9,4 +9,4 @@ cd target
 for filename in $(find . -name "*.msi" -o -name "*.rpm" -o -name "*.deb" -o -name "*.zip" -o -name "*.tar.gz");do
   echo "===> Uploading to GH $TAG: ${filename}"
       gh release upload $TAG $filename
-dones
+done
