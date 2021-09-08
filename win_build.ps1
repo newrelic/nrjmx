9b2498ea63fb589414cc7f2fd881e4116740a653
cristianciutea <cristi.ciutea@gmail.com>
2021-09-08 12:31:05 +0200

    test

--- win_build.ps1
+++ win_build.ps1
@@ -13,31 +13,34 @@ param (
     #[switch]$skipTests=$false
 )
 
-echo "Checking MSBuild.exe..."
-$msBuild = (Get-ItemProperty hklm:\software\Microsoft\MSBuild\ToolsVersions\4.0).MSBuildToolsPath
-if ($msBuild.Length -eq 0) {
-    echo "Can't find MSBuild tool. .NET Framework 4.0.x must be installed"
-    exit -1
-}
-echo $msBuild
-
-echo "--- Building Installer"
-
-Push-Location -Path "pkg\windows\"
-$env:NRJMX_VERSION = $version
-. $msBuild/MSBuild.exe nrjmx-installer.wixproj
-
-if (-not $?)
-{
-    echo "Failed building installer"
-    Pop-Location
-    exit -1
-}
-
-echo "Making versioned installed copy"
-
-cd ..\..\target\msi\Release
-
-cp "nrjmx.msi" "nrjmx-$arch.$version.msi"
-
-Pop-Location
+echo "test"
+ls
+#
+# echo "Checking MSBuild.exe..."
+# $msBuild = (Get-ItemProperty hklm:\software\Microsoft\MSBuild\ToolsVersions\4.0).MSBuildToolsPath
+# if ($msBuild.Length -eq 0) {
+#     echo "Can't find MSBuild tool. .NET Framework 4.0.x must be installed"
+#     exit -1
+# }
+# echo $msBuild
+#
+# echo "--- Building Installer"
+#
+# Push-Location -Path "pkg\windows\"
+# $env:NRJMX_VERSION = $version
+# . $msBuild/MSBuild.exe nrjmx-installer.wixproj
+#
+# if (-not $?)
+# {
+#     echo "Failed building installer"
+#     Pop-Location
+#     exit -1
+# }
+#
+# echo "Making versioned installed copy"
+#
+# cd ..\..\target\msi\Release
+#
+# cp "nrjmx.msi" "nrjmx-$arch.$version.msi"
+#
+# Pop-Location
