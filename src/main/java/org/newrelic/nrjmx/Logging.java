001d5b1eeb39b35e4b79e49b50af22bb8ea30b0e
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2021-09-15 09:22:51 -0500

    adds support for java.lang.Date and fixes problem related java.sql.Time

--- src/main/java/org/newrelic/nrjmx/Logging.java
+++ src/main/java/org/newrelic/nrjmx/Logging.java
@@ -1,26 +1,31 @@
 /*
-* Copyright 2020 New Relic Corporation. All rights reserved.
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package org.newrelic.nrjmx;
 
-import java.util.logging.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Logging {
-    public static void setup(Logger logger, boolean verbose) {
-        logger.setUseParentHandlers(false);
-        Handler consoleHandler = new ConsoleHandler();
-        logger.addHandler(consoleHandler);
 
-        consoleHandler.setFormatter(new SimpleFormatter());
+  public static void setup(Logger logger, boolean verbose) {
+    logger.setUseParentHandlers(false);
+    Handler consoleHandler = new ConsoleHandler();
+    logger.addHandler(consoleHandler);
 
-        if (verbose) {
-            logger.setLevel(Level.FINE);
-            consoleHandler.setLevel(Level.FINE);
-        } else {
-            logger.setLevel(Level.INFO);
-            consoleHandler.setLevel(Level.INFO);
-        }
+    consoleHandler.setFormatter(new SimpleFormatter());
+
+    if (verbose) {
+      logger.setLevel(Level.FINE);
+      consoleHandler.setLevel(Level.FINE);
+    } else {
+      logger.setLevel(Level.INFO);
+      consoleHandler.setLevel(Level.INFO);
     }
+  }
 }
