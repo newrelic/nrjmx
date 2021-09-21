d7ecd3d4ed8a4ef5a130a6181936069efdcdf4de
Grigorii Merkushev <brushknight@gmail.com>
2021-09-21 10:11:17 +0200

    ci: update release targets

--- build/release.mk
+++ build/release.mk
@@ -1,16 +1,18 @@
-.PHONY : package
-package:
+.PHONY : release/package
+release/package:
 	@($(MAVEN_BIN) versions:set -DnewVersion=\$(subst v,,$(TAG)))
 	@($(MAVEN_BIN) clean package -DskipTests)
 
-.PHONY : sign
+.PHONY : release/sign
 release/sign:
 	@echo "=== [sign] signing packages"
 	@bash $(CURDIR)/build/sign.sh
 
-publish:
+.PHONY : release/publish
+release/publish:
 	@echo "=== [release/publish] publishing artifacts"
 	@bash $(CURDIR)/build/upload_artifacts_gh.sh
 
-release: package sign publish
+.PHONY : release
+release: release/package release/sign release/publish
 	@echo "=== [release] full pre-release cycle complete for nix"
