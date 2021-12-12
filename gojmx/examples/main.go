173bfc8ea584807e242c2acc0d0073080f505bc9
cristianciutea <cristi.ciutea@gmail.com>
2021-12-12 18:15:55 +0100

    cleanup example

--- gojmx/examples/main.go
+++ gojmx/examples/main.go
@@ -8,65 +8,68 @@ import (
 	"path/filepath"
 )
 
-func main() {
-
+func init() {
+	// Uncomment this when you want use the nrjmx.jar build from the project bin directory.
 	os.Setenv("NR_JMX_TOOL", filepath.Join("..", "bin", "nrjmx"))
-	//os.Setenv("NRIA_NRJMX_DEBUG", "true")
 
+	// Uncomment this when you want to run both: golang debugger and java debugger.
+	//os.Setenv("NRIA_NRJMX_DEBUG", "true")
+}
+
+func main() {
+	// JMX Client configuration.
 	config := &gojmx.JMXConfig{
 		Hostname:        "localhost",
 		Port:            7199,
-		//RequestTimoutMs: 10000,
+		RequestTimoutMs: 10000,
 	}
 
+	// Connect to JMX endpoint.
 	client, err := gojmx.NewClient(context.Background()).Open(config)
-	jmxErr, ok := gojmx.IsJMXError(err)
-	if ok {
-		fmt.Println("here", jmxErr.Message)
+	handleError(err)
+
+	// Query the mBean names.
+	mBeanNames, err := client.GetMBeanNames("java.lang:type=*")
+	handleError(err)
+
+	// Query the Attribute names for each mBeanName.
+	for _, mBeanName := range mBeanNames {
+		mBeanAttrNames, err := client.GetMBeanAttrNames(mBeanName)
+		handleError(err)
+
+		for _, mBeanAttrName := range mBeanAttrNames {
+			// Query the attribute value for each mBeanName and mBeanAttributeName.
+			jmxAttrs, err := client.GetMBeanAttrs(mBeanName, mBeanAttrName)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			for _, jmxAttr := range jmxAttrs {
+				printAttr(jmxAttr)
+			}
+		}
 	}
-
-	err = client.Close()
-	fmt.Println(err)
-
-
-	//result, err := client.GetMBeanAttrs("java.lang:type=Memory", "HeapMemoryUsage")
-
-
-	j2, ok := gojmx.IsJMXError(err)
-	if ok {
-		fmt.Println("here", j2.Message)
-	} else {
-		//panic(err)
-	}
-	//data := map[string]interface{} {"test": result[0]}
-	//b, _ := json.Marshal(data)
-	//fmt.Println(string(b))
-	//time.Sleep(1*time.Hour)
 }
 
-//
-//func main() {
-//	ctx := context.Background()
-//
-//	client, err := gojmx.NewJMXServiceClient(ctx)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	err = client.Connect(ctx, &nrprotocol.JMXConfig{
-//		Hostname: "localhost",
-//		Port:     7199,
-//		UriPath:  "jmxrmi",
-//	})
-//	if err != nil {
-//		panic(err)
-//	}
-//	defer client.Disconnect(ctx)
-//
-//	result, err := client.QueryMbean(ctx, "*:*")
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	fmt.Println(result)
-//}
+func printAttr(jmxAttr *gojmx.JMXAttribute) {
+	fmt.Fprintf(
+		os.Stdout,
+		"Attribute Name: %s\nAttribute Value: %v\nAttribute Value Type: %v\n\n",
+		jmxAttr.Attribute,
+		jmxAttr.GetValue(),
+		jmxAttr.ValueType,
+	)
+}
+
+func handleError(err error) {
+	if jmxErr, ok := gojmx.IsJMXError(err); ok {
+		fmt.Println("JMXError message:", jmxErr.Message)
+		fmt.Println("JMXError stacktrace:", jmxErr.Stacktrace)
+		os.Exit(1)
+	} else if jmxConnErr, ok := gojmx.IsJMXConnectionError(err); ok {
+		fmt.Println("JMXConnectionError message:", jmxConnErr.Message)
+		os.Exit(2)
+	} else if err != nil {
+		panic(err)
+	}
+}
