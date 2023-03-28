eee71b455118314a0a19deaaf0f1fc14df8edda4
Cristian Ciutea <cristi.ciutea@gmail.com>
2023-03-28 11:08:55 +0200

    test

--- gojmx/examples/main.go
+++ gojmx/examples/main.go
@@ -65,7 +65,7 @@ func main() {
 
 	// Or use QueryMBean call which wraps all the necessary requests to get the values for an MBeanNamePattern.
 	// Optionally you can provide atributes to QueryMBeanAttributes in tha same way you provide for GetMBeanAttributes,
-	// e.g.: response, err := client.QueryMBeanAttributes("java.lang:type=*", mBeanAttrNames...)
+	// e.g.: response, err := client.QueryMBeanAttributes("java.lang:type=*", mBeanAttrNames...).
 	response, err := client.QueryMBeanAttributes("java.lang:type=*")
 	handleError(err)
 	for _, attr := range response {
